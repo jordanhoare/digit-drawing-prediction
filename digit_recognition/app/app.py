6a78822ed2036e4621c44dbace27b9b5d638b53d
jordanhoare <jordanhoare0@gmail.com>
2022-01-09 13:37:17 +1000

    configure sqlite and routes for jinja

---
+++ digit_recognition/app/app.py
@@ -0,0 +1,93 @@
+from typing import Dict, List
+
+from config import settings
+from fastapi import BackgroundTasks, Depends, FastAPI, HTTPException, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+from pydantic.main import BaseModel
+from sqlalchemy.orm import Session
+
+from . import models
+from .database import SessionLocal, engine
+from .models import Images
+
+# Create db & table with SQLalchemy
+models.Base.metadata.create_all(bind=engine)
+
+
+# Dependency
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+# Pydantic classes
+class ImageRequest(BaseModel):
+    phrase: str
+
+
+class ImageResponse(BaseModel):
+    probabilities: Dict[str, float]
+    sentiment: str
+    confidence: float
+
+
+# FastAPI
+app = FastAPI(
+    title=settings.name, description=settings.description, version=settings.version
+)
+
+# Jinja2Templates
+templates = Jinja2Templates(
+    directory="D:/CompSci/Projects/digit-drawing-prediction/digit_recognition/templates/"
+)
+
+
+# Routes
+
+
+@app.get("/", response_class=HTMLResponse)
+def dashboard(
+    request: Request,
+    db: Session = Depends(get_db),
+):
+    """
+    -------
+    """
+    images = db.query(Images).all()
+    return templates.TemplateResponse(
+        "home.html",
+        {
+            "request": request,
+            "images": images,
+        },
+    )
+
+
+@app.post("/image")
+def create_image(
+    image_request: ImageRequest,
+    db: Session = Depends(get_db),
+):
+    """
+    background_tasks: BackgroundTasks,
+    background_tasks.add_task(predict_image, image.id)
+
+    ,
+    (2) add database record
+    (3) give background_tasks a reference of image record
+    """
+    image = Images()
+    image.image = image_request.image
+
+    db.add(image)
+    db.commit()
+
+    return {
+        "code": "success",
+        "message": "image added",
+    }
